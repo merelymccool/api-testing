dede145e96e0dba93bfd62e73bfbe8d443fb4571
Dayle McCool <mccool@dayles-macbook.home>
2024-10-02 19:10:47 -0400

    refactor tests

--- python-cats/tests/test_cats_api.py
+++ python-cats/tests/test_cats_api.py
@@ -1,18 +1,24 @@
 import pytest
+import json
 
 from cats_api.main import SimpleRequest
 
-def test_status_code():
-	cats = SimpleRequest()
+cats = SimpleRequest()
 
+def test_status_code():
 	result = cats.response("https://api.thecatapi.com/v1/images/search")
 
 	assert result == 200
 
 def test_response_length():
-	cats = SimpleRequest()
-
 	result = cats.body("https://api.thecatapi.com/v1/images/search")
 
 	assert len(result) == 1
+
+def test_body_data():
+	result = cats.body("https://api.thecatapi.com/v1/images/search")
+
+	result_json = json.dumps(result, indent = 4)
+
+	assert "https://cdn2.thecatapi.com/images/" in result_json
 
