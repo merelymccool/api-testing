b93ccada8089039b31a67d7c6ab91d50b660a859
Dayle McCool <mccool.dm@gmail.com>
2024-10-07 22:23:06 -0400

    add status, response tests

--- js-booker/test/test_booker.js
+++ js-booker/test/test_booker.js
@@ -2,14 +2,21 @@ var assert = require('assert');
 const apiUrl = 'https://restful-booker.herokuapp.com/booking'
 
 describe('API Testing restful=booker', function () {
-	it('should pass', function () {
+	it('should have status 200', function () {
 		fetch(apiUrl)
 		.then(response => {
-			return response.json();
-		})
-		.then(data => {
-			assert(data);
+			assert.equal(response.status,200);
 		});
 	});
+
+	it('should not have empty data', function () {
+                fetch(apiUrl)
+                .then(response => {
+			return response.json()
+                })
+		.then(data => {
+			assert.isNotEmpty(data);
+		});
+        });
 });
 
