d454db3295bef2256f6bb830d605a406b10dd431
Dayle McCool <mccool@Dayles-Macbook.local>
2024-10-07 20:08:54 -0400

    add breeds endpoint test

--- python-cats/tests/test_cats_api.py
+++ python-cats/tests/test_cats_api.py
@@ -5,20 +5,25 @@ from cats_api.main import SimpleRequest
 
 cats = SimpleRequest()
 
-def test_status_code():
+def test_image_status_code():
 	result = cats.send("meta","https://api.thecatapi.com/v1/images/search")
 
 	assert result == 200
 
-def test_response_length():
+def test_image_response_length():
 	result = cats.send("body","https://api.thecatapi.com/v1/images/search")
 
 	assert len(result) == 1
 
-def test_body_data():
+def test_image_body_data():
 	result = cats.send("body","https://api.thecatapi.com/v1/images/search")
 
 	result_json = json.dumps(result, indent = 4)
 
 	assert "https://cdn2.thecatapi.com/images/" in result_json
+
+def test_breed_status_code():
+	result = cats.send("meta","https://api.thecatapi.com/v1/breeds")
+
+	assert result == 200
 
